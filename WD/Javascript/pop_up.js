631cde80c3276f083132b0739a41b2cfa6719ac3
SatyamH13 <satyamhirpara.tops@gmail.com>
2022-11-13 22:00:13 -0800

    Program

---
+++ WD/Javascript/pop_up.js
@@ -0,0 +1,7 @@
+document.write("hello satyam");
+console.log("how are you ?");
+document.getElementById('h').innerHTML = "Good Morning";
+window.alert("This is Alert Box");
+confirm("are you sure ?");
+prompt("5 + 5 = ?");
+print("");
